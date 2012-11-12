67f43432f8406c2cb8960db5eb86d6ee95ca3dbf
Jason Cooper <jase.coop@gmail.com>
2012-11-12 14:28:16 +0000

    First commit, project finished! Fuck yeah.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,3 @@
-//= require_tree .
+//= require jquery-1.8.2.min
+//= require garlic
+//= require public
---
+++ source/javascripts/public.js
@@ -0,0 +1,35 @@
+var introNeed = function(){
+
+  $('textarea#main').fadeIn();
+  var text = $('textarea#main').val();
+  
+  if(text == ''){
+
+    $('.intro').fadeIn();
+    introFade();
+
+  }
+}
+
+var introFade = function(){
+
+  $('.intro').click(function(){
+
+    $(this).fadeOut();
+
+    $('textarea#main').focus();
+
+  });
+
+}
+
+$(window).load(function() {
+
+introNeed();
+
+});
+
+$(document).ready(function() {
+
+
+});
